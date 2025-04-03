4a5863bebf5dffa5c201a2c4f82fc5444d9ca697
Jasi-coder <jasmin.zel@gmx.de>
2025-04-03 09:08:49 +0200

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\dev\\bin\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\dev\\repo\\Schulprojekt-Kostenerfassung-fuer-den-Haushalt" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\dev\\bin\\flutter"
+  "PROJECT_DIR=C:\\dev\\repo\\Schulprojekt-Kostenerfassung-fuer-den-Haushalt"
+  "FLUTTER_ROOT=C:\\dev\\bin\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\dev\\repo\\Schulprojekt-Kostenerfassung-fuer-den-Haushalt\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\dev\\repo\\Schulprojekt-Kostenerfassung-fuer-den-Haushalt"
+  "FLUTTER_TARGET=lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=true"
+  "PACKAGE_CONFIG=C:\\dev\\repo\\Schulprojekt-Kostenerfassung-fuer-den-Haushalt\\.dart_tool\\package_config.json"
+)
